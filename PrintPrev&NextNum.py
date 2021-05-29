e4405fd4ca79eec6c655f3b6d0bd18bab6b85494
ATHULKNAIR <athul2639@gmail.com>
2021-05-29 21:14:18 +0530

    Print Previous and Next Number

---
+++ PrintPrev&NextNum.py
@@ -0,0 +1,8 @@
+# Write a program that reads an integer number and prints its previous and next
+# numbers. See the example below
+
+
+a = int(input('Enter a Number'))
+
+print("The next number for the number ",a," is ",a+1)
+print("The previous number for the number ",a," is ",a-1)
