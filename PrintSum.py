e3d8a141a1a75577dad7be32b33a04d09c96aea9
ATHULKNAIR <athul2639@gmail.com>
2021-05-29 21:14:45 +0530

    Print Sum of three numbers

---
+++ PrintSum.py
@@ -0,0 +1,9 @@
+# Write a program that takes three numbers and prints their sum. Every number
+# is given on a separate line.
+
+
+a = int(input('Enter First Number'))
+b = int(input('Enter Second Number'))
+c = int(input('Enter Third Number')) 
+
+print(a + b + c)
