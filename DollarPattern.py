304856906ea87ca2a098712187f97aad7d09fe8f
ATHULKNAIR <athul2639@gmail.com>
2021-06-07 19:33:36 +0530

    Pattern

---
+++ DollarPattern.py
@@ -0,0 +1,16 @@
+# Write a program to produce the output
+# $ $ $ $
+# $ $ $ $
+# $ $ $ $
+# $ $ $ $
+# $ $ $ $
+
+
+
+for i in range(0, 4):  
+       
+        for j in range(0, 4):  
+            
+            print("$ ",end = "")       
+ 
+        print()
